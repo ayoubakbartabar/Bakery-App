7d454c543e2f3bc290d5b4a9be461bdd08885133
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-17 18:41:19 +0330

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,12 @@ import { React, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "../pages/HomePage/HomePage.jsx";
+import AboutPage from "../pages/AboutPage/AboutPage.jsx";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/about" element={<About />} /> */}
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   );
 }
