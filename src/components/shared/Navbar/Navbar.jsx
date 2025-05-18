d663c246583332d7f4de27c95b02b2a459d81f16
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-18 09:23:26 +0330

    Update Navbar.jsx

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -3,21 +3,20 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const MenuArr = [
-    { id: 1, title: "About", href: "/about" },
-    { id: 2, title: "Projects", href: "/projects" },
-    { id: 3, title: "Contact", href: "/contact" },
+  const leftMenu = [
+    { id: 1, title: "HOME PAGE", href: "/" },
+    { id: 2, title: "ABOUT", href: "/about" },
+    { id: 3, title: "PRODUCT", href: "/product" },
+    { id: 4, title: "PAGES", href: "/pages" },
+  ];
+
+  const rightMenu = [
+    { id: 5, title: "GALLERY", href: "/gallery" },
+    { id: 6, title: "BLOGS", href: "/blogs" },
+    { id: 7, title: "CONTACT", href: "/contact" },
   ];
 
   return (
-    <nav className="navbar-section">
-      <ul className="navbar-menu">
-        {MenuArr.map((item) => (
-          <li key={item.id}>
-            <Link to={item.href}>{item.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+
   );
 }
