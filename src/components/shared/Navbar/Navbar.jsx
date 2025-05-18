779abcf23ee648693b6ce3059f55057f37f0ea23
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-18 09:25:36 +0330

    Update Navbar.jsx

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -4,16 +4,15 @@ import "./Navbar.css";
 
 export default function Navbar() {
   const leftMenu = [
-    { id: 1, title: "HOME PAGE", href: "/" },
-    { id: 2, title: "ABOUT", href: "/about" },
-    { id: 3, title: "PRODUCT", href: "/product" },
-    { id: 4, title: "PAGES", href: "/pages" },
+    { id: 1, title: "ABOUT", href: "/about" },
+    { id: 2, title: "PRODUCT", href: "/product" },
+    { id: 3, title: "PAGES", href: "/pages" },
   ];
 
   const rightMenu = [
-    { id: 5, title: "GALLERY", href: "/gallery" },
-    { id: 6, title: "BLOGS", href: "/blogs" },
-    { id: 7, title: "CONTACT", href: "/contact" },
+    { id: 4, title: "GALLERY", href: "/gallery" },
+    { id: 5, title: "BLOGS", href: "/blogs" },
+    { id: 6, title: "CONTACT", href: "/contact" },
   ];
 
   return (
