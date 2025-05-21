727d0b2c59205f98b4f586b86f5b2b3d4c558381
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-21 15:49:57 +0330

    Update NavBar.jsx : set Hook for shopping cart , create test array , create const for checking total

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { RiShoppingBasket2Line } from "react-icons/ri";
 import { CiSearch } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
 
 import "./Navbar.css";
 import SearchModal from "./SearchModal/SearchModal";
@@ -9,6 +10,7 @@ import SearchModal from "./SearchModal/SearchModal";
 export default function Navbar() {
   // set Hook for Search bar
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [cartHover, setCartHover] = useState(false);
 
   const leftMenu = [
     { id: 1, title: "ABOUT", href: "/about" },
@@ -22,6 +24,21 @@ export default function Navbar() {
     { id: 6, title: "CONTACT", href: "/contact" },
   ];
 
+  // this array for test
+  const cartItems = [
+    {
+      id: 1,
+      title: "Oreo Cupcakes",
+      price: 36,
+      quantity: 5,
+      image: "https://via.placeholder.com/50",
+    },
+  ];
+
+  const cartTotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
   return (
     <>
       <nav className="navbar">
@@ -45,6 +62,7 @@ export default function Navbar() {
               <Link to={item.href}>{item.title}</Link>
             </li>
           ))}
+
           <li>
             <button
               onClick={() => setIsSearchOpen(true)}
@@ -53,14 +71,24 @@ export default function Navbar() {
               <CiSearch className="searchIcon" />
             </button>
           </li>
-          <li>
+
+          <li
+            className="cart-wrapper"
+            onMouseEnter={() => setCartHover(true)}
+            onMouseLeave={() => setCartHover(false)}
+          >
             <button className="icon-btn cart">
               <RiShoppingBasket2Line className="shoppingIcon" />
-              <span className="badge">0</span>
+              <span className="badge">{cartItems.length}</span>
             </button>
+
+            {cartHover && (
+            
+            )}
           </li>
         </ul>
       </nav>
+
       {isSearchOpen && <SearchModal onClose={() => setIsSearchOpen(false)} />}
     </>
   );
