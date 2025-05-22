e4481dd2afe68a55c8cca81780ca010b3fc1f1ac
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-22 08:51:26 +0330

    Update App.jsx  : set new routs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 import HomePage from "../pages/HomePage/HomePage.jsx";
 import AboutPage from "../pages/AboutPage/AboutPage.jsx";
 import PagesPage from "../pages/PagesPage/PagesPage.jsx";
+import GalleryPage from "../pages/GalleryPage/GalleryPage.jsx";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/pages" element={<PagesPage/>} />
+      <Route path="/gallery" element={<GalleryPage/>} />
     </Routes>
   );
 }
