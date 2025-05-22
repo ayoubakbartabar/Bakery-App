5701ba087359ccf3b081974532ae858a82ca7e9c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-22 08:49:07 +0330

    Update Pages.jsx and App.jsx  : rename the pages components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "../pages/HomePage/HomePage.jsx";
 import AboutPage from "../pages/AboutPage/AboutPage.jsx";
-import Pages from "../pages/Pages/Pages.jsx";
+import PagesPage from "../pages/PagesPage/PagesPage.jsx";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/pages" element={<Pages/>} />
+      <Route path="/pages" element={<PagesPage/>} />
     </Routes>
   );
 }
--- src/components/pages/PagesPage/PagesPage.jsx
+++ src/components/pages/PagesPage/PagesPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../shared/Navbar/Navbar.jsx'
 
-export default function Pages() {
+export default function PagesPage() {
   return (
     <Navbar/>
   )
