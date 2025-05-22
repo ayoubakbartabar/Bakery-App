5b6e001b32c5200767a2e942969497ff0c228a0e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-22 08:59:36 +0330

    Update App.jsx  : set new routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,11 +7,14 @@ import PagesPage from "../pages/PagesPage/PagesPage.jsx";
 import GalleryPage from "../pages/GalleryPage/GalleryPage.jsx";
 import BlogsPage from "../pages/BlogsPage/BlogsPage.jsx";
 import ContactPage from "../pages/ContactPage/ContactPage.jsx";
+import ProductPage from "../pages/ProductPage/ProductPage.jsx";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/about" element={<ProductPage />} />
       <Route path="/pages" element={<PagesPage/>} />
       <Route path="/gallery" element={<GalleryPage/>} />
       <Route path="/blogs" element={<BlogsPage/>} />
