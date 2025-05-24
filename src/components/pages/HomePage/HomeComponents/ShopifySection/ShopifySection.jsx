25b87723d8e76cce22edb73fc6a2a416e7916d88
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-24 17:44:47 +0330

    Update ShopifySection.jsx : create slider and set program

--- src/components/pages/HomePage/HomeComponents/ShopifySection/ShopifySection.jsx
+++ src/components/pages/HomePage/HomeComponents/ShopifySection/ShopifySection.jsx
@@ -1,10 +1,41 @@
-import React , {useState} from 'react'
-import './ShopifySection.css'
+import React, { useState } from "react";
+import "./ShopifySection.css";
+import ShopifyData from "./ShopifySectionData";
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from "react-icons/md";
 
 export default function ShopifySection() {
-  return (
-    <section className='shopify-section'>
+  // Set Hook
+  const [current, setCurrent] = useState(0);
+  const length = ShopifyData.length;
+  // create arrows handler
+  const prevSlider = () => {
+    setCurrent((prev) => (prev - 1 + length) % length);
+  };
 
+  const nextSlider = () => {
+    setCurrent((prev) => (prev + 1) % length);
+  };
+
+  return (
+    <section className="shopify-section">
+      <div className="slider-container">
+        <button className="nav-button left" onClick={prevSlider}>
+          <MdOutlineKeyboardArrowLeft size={32} />
+        </button>
+
+        <div className="slider-item">
+          <img src={ShopifyData[current].img} alt={ShopifyData[current].alt} />
+        </div>
+
+        <button className="nav-button right" onClick={nextSlider}>
+          <MdOutlineKeyboardArrowRight size={32} />
+        </button>
+      </div>
+
+      <button className="add-to-cart">ADD TO CART</button>
     </section>
-  )
+  );
 }
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './HomePage.css'
 import Navbar from '../../shared/Navbar/Navbar'
+import ShopifySection from './HomeComponents/ShopifySection/ShopifySection'
 export default function HomePage() {
   return (
     <>
       <Navbar/>
+      <ShopifySection/>
     </>
   )
 }
