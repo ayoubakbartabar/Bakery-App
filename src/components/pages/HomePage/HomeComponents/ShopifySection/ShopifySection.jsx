5e0d519079546e6690b93b627a38b1a9f73a09bd
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-24 17:21:10 +0330

    Create ShopifySection component for HomePage component

---
+++ src/components/pages/HomePage/HomeComponents/ShopifySection/ShopifySection.jsx
@@ -0,0 +1,10 @@
+import React , {useState} from 'react'
+import './ShopifySection.css'
+
+export default function ShopifySection() {
+  return (
+    <section className='shopify-section'>
+
+    </section>
+  )
+}
---
+++ src/components/pages/HomePage/HomeComponents/ShopifySection/ShopifySectionData.js
@@ -0,0 +1,24 @@
+const ShopifyData = [
+  {
+    id: 1,
+    alt: "Lemon Cupcakes",
+    img: "src/assets/images/slider-5_569b9362-caa3-4772-a204-82017d155969.webp",
+  },
+  {
+    id: 2,
+    alt: "Moist Chocolate",
+    img: "src/assets/images/5_7cd03533-622c-46b5-80eb-5f4b833a9799_grande.webp",
+  },
+  {
+    id: 3,
+    alt: "Pumpkin Spice",
+    img: "src/assets/images/6_e896a676-d4b2-4188-a4e0-017f7656bada_grande.webp",
+  },
+  {
+    id: 4,
+    alt: "Moist Chocolate",
+    img: "src/assets/images/slider-6_ea5a07b1-22bd-40f0-9efa-fe36bc9e7840.webp",
+  },
+];
+
+export default ShopifyData;
