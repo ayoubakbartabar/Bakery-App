8e2c53d03db538c602a0031b7a846e3ad9ed0a0a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-25 16:24:51 +0330

    Update WelcomeSection.jsx and styles : create component tags and set styles for this ...

--- src/components/pages/HomePage/HomeComponents/WelcomeSection/WelcomeSection.jsx
+++ src/components/pages/HomePage/HomeComponents/WelcomeSection/WelcomeSection.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './WelcomeSection.css'
+import React from "react";
+import "./WelcomeSection.css";
+import WelcomeSectionData from "./WelcomeSectionData";
 
 export default function WelcomeSection() {
   return (
-    <section className='welcome-section'>
-        
+    <section className="welcome-section">
+      <div className="welcome-section-top">
+        <h1 className="welcome-section-top-title">Delicieux</h1>
+        <p className="welcome-section-top-paragraph">WELCOME TO THE STORE</p>
+        <img
+          className="welcome-section-top-img"
+          src="src/assets/images/floral_grande.avif"
+          alt="floral_grande"
+        />
+      </div>
+      <div className="welcome-section-bottom">
+        {WelcomeSectionData.map((item) => (
+          <div className="welcome-section-box" key={item.id}>
+            <img className="welcome-section-box-svg" src={item.svg} alt="svg" />
+            <h2 className="welcome-section-box-title">{item.title}</h2>
+            <h3 className="welcome-section-box-date">{item.date}</h3>
+            <p className="welcome-section-box-paragraph">{item.paragraph}</p>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
