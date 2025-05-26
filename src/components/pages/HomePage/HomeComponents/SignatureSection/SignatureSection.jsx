37b2d13783aaa80dfeb92e0e29905c8737371cea
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-26 16:50:26 +0330

    Update SignatureSection.jsx : create SignatureSection tags

--- src/components/pages/HomePage/HomeComponents/SignatureSection/SignatureSection.jsx
+++ src/components/pages/HomePage/HomeComponents/SignatureSection/SignatureSection.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import './SignatureSection.css'
+import React from "react";
+import "./SignatureSection.css";
 
 export default function SignatureSection() {
   return (
-    
-    <section className="signature-section"></section>
-
-    )
-
+    <section className="signature-section">
+      <img
+        className="signature-section-img"
+        src="src\assets\images\signature_grande.webp"
+        alt="signature image"
+      />
+      <div className="signature-section-left-side">
+        <h2 className="signature-section-left-side-title">
+          “It seems that every country that can get its hands on butter has its
+          opinion of what butter cream frosting should be. Some are made with
+          eggs and butter.”
+        </h2>
+        <p className="signature-section-left-side-paragraph">
+          Sunshine - CEO Bakery
+        </p>
+        <img
+          className="signature-section-left-side-image"
+          src="src\assets\images\signature-2_grande.png"
+          alt="signature svg"
+        />
+      </div>
+    </section>
+  );
 }
