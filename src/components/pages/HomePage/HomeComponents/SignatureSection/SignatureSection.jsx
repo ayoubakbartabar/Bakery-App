b6009ed0227aeb9de15a60a3388eeef9c6a1b48a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-26 16:45:55 +0330

    Create SignatureSection component

---
+++ src/components/pages/HomePage/HomeComponents/SignatureSection/SignatureSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SignatureSection.css'
+
+export default function SignatureSection() {
+  return (
+    
+    <section className="signature-section"></section>
+
+    )
+
+}
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -3,12 +3,14 @@ import './HomePage.css'
 import Navbar from '../../shared/Navbar/Navbar'
 import ShopifySection from './HomeComponents/ShopifySection/ShopifySection'
 import WelcomeSection from './HomeComponents/WelcomeSection/WelcomeSection'
+import SignatureSection from './HomeComponents/SignatureSection/SignatureSection'
 export default function HomePage() {
   return (
     <>
       <Navbar/>
       <ShopifySection/>
       <WelcomeSection/>
+      <SignatureSection/>
     </>
   )
 }
