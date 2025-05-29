23acb6e53a3743f95dde95f1d067415ed24cab1a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-29 15:12:07 +0330

    Create CarouselSectionData.js : create data array and set properties

---
+++ src/components/pages/HomePage/HomeComponents/CarouselSection/CarouselSectionData.js
@@ -0,0 +1,31 @@
+const CarouselSectionData = [
+  {
+    id: 1,
+    image: "src/assets/images/1_152x152_crop_center.avif",
+    allstars: 5,
+    rate: 4,
+    text: "“Dessert pudding dessert jelly beans cupcake sweet caramels gingerbread. Fruitcake biscuit cheesecake. Cookie topping sweet muffin pudding tart bear claw sugar plum croissant.”",
+    name: "Jane",
+    role: " - Manager",
+  },
+  {
+    id: 2,
+    image: "src/assets/images/2_152x152_crop_center.avif",
+    allstars: 5,
+    rate: 5,
+    text: "“Dessert pudding dessert jelly beans cupcake sweet caramels gingerbread. Fruitcake biscuit cheesecake. Cookie topping sweet muffin pudding tart bear claw sugar plum croissant.”",
+    name: "anna",
+    role: " - designer",
+  },
+  {
+    id: 3,
+    image: "src/assets/images/4_152x152_crop_center.avif",
+    allstars: 5,
+    rate: 5,
+    text: "“Dessert pudding dessert jelly beans cupcake sweet caramels gingerbread. Fruitcake biscuit cheesecake. Cookie topping sweet muffin pudding tart bear claw sugar plum croissant.”",
+    name: "lily",
+    role: " - accounting",
+  },
+];
+
+export default CarouselSectionData;
