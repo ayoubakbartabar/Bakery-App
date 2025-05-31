418d16d0392ebfba1bd8a81167d9bfd2d73d6ced
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-31 14:41:59 +0330

    Create DeliverySection components

---
+++ src/components/pages/HomePage/HomeComponents/DeliverySection/DeliverySection.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './DeliverySection.css'
+
+export default function DeliverySection() {
+  return (
+    <section className='delivery-section'>DeliverySection</section>
+  )
+}
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -6,9 +6,10 @@ import WelcomeSection from "./HomeComponents/WelcomeSection/WelcomeSection";
 import SignatureSection from "./HomeComponents/SignatureSection/SignatureSection";
 import SomeProducts from "./HomeComponents/SomeProducts/SomeProducts";
 import CarouselSection from "./HomeComponents/CarouselSection/CarouselSection.jsx";
+import HistorySection from "./HomeComponents/HistorySection/HistorySection.jsx";
+import DeliverySection from "./HomeComponents/DeliverySection/DeliverySection.jsx";
 
 import { BuyProductsProvider } from "../../shared/BuyProductContext/BuyProductContext.jsx";
-import HistorySection from "./HomeComponents/HistorySection/HistorySection.jsx";
 
 export default function HomePage() {
   return (
@@ -21,6 +22,7 @@ export default function HomePage() {
         <SomeProducts />
         <CarouselSection />
         <HistorySection/>
+        <DeliverySection/>
       </BuyProductsProvider>
     </>
   );
