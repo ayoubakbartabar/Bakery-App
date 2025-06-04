2e917713df559f9e11fe737cf2c7b6881025a65b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-04 14:48:53 +0330

    Update AboutNumberSection.jsx : create tags ..

--- src/components/pages/AboutPage/AboutPageComponents/AboutNumberSection/AboutNumberSection.jsx
+++ src/components/pages/AboutPage/AboutPageComponents/AboutNumberSection/AboutNumberSection.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './AboutNumberSection.css'
+import React from "react";
+import AboutNumberData from "./AboutNumberData";
+import "./AboutNumberSection.css";
 
 export default function AboutNumberSection() {
   return (
-    <section className='about-number-section'>
-        
+    <section className="about-number-section">
+      {AboutNumberData.map((item) => (
+        <div className="about-number-box" key={item.id}>
+          <i className={`icon ${item.iconClass}`}></i>
+          <h3 className="number">{item.number}</h3>
+          <p className="title">{item.title}</p>
+        </div>
+      ))}
     </section>
-  )
+  );
 }
