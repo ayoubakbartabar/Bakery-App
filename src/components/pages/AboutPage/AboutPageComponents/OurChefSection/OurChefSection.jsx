ca3006b34cabe35aaf49ef32cc66bcc5c089b61b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-05 16:43:34 +0330

    Create OurChefSection Component: create style js and jsx file...

---
+++ src/components/pages/AboutPage/AboutPageComponents/OurChefSection/OurChefSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './OurChefSection.css'
+import OurChefData from './OurChefData'
+
+
+export default function OurChefSection() {
+  return (
+    <section className='our-chef-section'>OurChefSection</section>
+  )
+}
