da2581a5327c616f362c4572b2fa5c91cdca6c0c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-05 16:49:29 +0330

    Update OurChefData.js : create array properties and set data ....

--- src/components/pages/AboutPage/AboutPageComponents/OurChefSection/OurChefData.js
+++ src/components/pages/AboutPage/AboutPageComponents/OurChefSection/OurChefData.js
@@ -1,3 +1,40 @@
-const OurChefData = []
+const OurChefData = [
+  {
+    id: 1,
+    image: "src/assets/images/1_1_grande.webp",
+    title: "Christian Gregory",
+    role: "CEO - Founder",
+    paragraph: "“Jelly topping halvah caramels sweet cake gummi bears toffee.”",
+    tel: "947 577 57",
+    url: "juanmaster@bready.com",
+  },
+  {
+    id: 2,
+    image: "src/assets/images/2_grande.webp",
+    title: "Christian Gregory",
+    role: "CEO - Founder",
+    paragraph: "“Jelly topping halvah caramels sweet cake gummi bears toffee.”",
+    tel: "947 57 57",
+    url: "juanmaster@bready.com",
+  },
+  {
+    id: 3,
+    image: "src/assets/images/3_grande.webp",
+    title: "Christian Gregory",
+    role: "CEO - Founder",
+    paragraph: "“Jelly topping halvah caramels sweet cake gummi bears toffee.”",
+    tel: "947 577 57",
+    url: "juanmaster@bready.com",
+  },
+  {
+    id: 4,
+    image: "src/assets/images/4_grande.webp",
+    title: "Christian Gregory",
+    role: "CEO - Founder",
+    paragraph: "“Jelly topping halvah caramels sweet cake gummi bears toffee.”",
+    tel: "947 577 57",
+    url: "juanmaster@bready.com",
+  },
+];
 
 export default OurChefData;
