f6dd1e76d7a0e9938311882eeeff7b86966a2261
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-07 16:40:03 +0330

    Update AboutNumberSection.jsx : set react animatine syntax.....

--- src/components/pages/AboutPage/AboutPageComponents/AboutNumberSection/AboutNumberSection.jsx
+++ src/components/pages/AboutPage/AboutPageComponents/AboutNumberSection/AboutNumberSection.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import AboutNumberData from "./AboutNumberData";
 import "./AboutNumberSection.css";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 export default function AboutNumberSection() {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
   return (
-    <section className="about-number-section">
+    <section className="about-number-section" ref={ref}>
       {AboutNumberData.map((item) => (
         <div key={item.id} className="about-number-item">
-          <i className={item.iconClass}></i>
-          <h3 className="about-number-number">{item.number}</h3>
+          <img src={item.svg} alt={item.title} className="about-number-image" />
+          <h3 className="about-number-number">
+            {inView ? <CountUp end={item.number} duration={2} /> : 0}
+          </h3>
           <p className="about-number-title">{item.title}</p>
         </div>
       ))}
