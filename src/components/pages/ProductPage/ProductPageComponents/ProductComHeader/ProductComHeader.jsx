f66d4c4757732f63aff521a97451546a573e155a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-08 15:25:30 +0330

    Create ProductComHeader componet :)

---
+++ src/components/pages/ProductPage/ProductPageComponents/ProductComHeader/ProductComHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProductComHeader.css'
+
+export default function ProductComHeader() {
+  return (
+    <section className='product-com-header'>
+
+    </section>
+  )
+}
