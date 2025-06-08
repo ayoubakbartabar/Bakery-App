b30c3149d30bca381b2374d96f87a26d7fc6d14c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-08 15:35:54 +0330

    Update ProductComHeader.css , ProductPage.jsx and image : set new styles and add to ProductPage components and add new images...

--- src/components/pages/ProductPage/ProductPage.jsx
+++ src/components/pages/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../shared/Navbar/Navbar";
 import Footer from "../../shared/Footer/Footer";
 import { BuyProductsProvider } from "../../shared/BuyProductContext/BuyProductContext";
+import ProductComHeader from "./ProductPageComponents/ProductComHeader/ProductComHeader";
 import "./ProductPage.css";
 
 export default function ProductPage() {
@@ -9,7 +10,7 @@ export default function ProductPage() {
     <>
       <BuyProductsProvider>
         <Navbar />
-
+        <ProductComHeader />
         <Footer />
       </BuyProductsProvider>
     </>
