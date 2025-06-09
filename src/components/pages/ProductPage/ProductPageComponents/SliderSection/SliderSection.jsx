a7e6ded52f515fc3678a228b9ab7b60ff10ff5bd
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-09 14:59:31 +0330

    Update SliderSection.jsx : create SliderSection.jsx tags and functions ...

--- src/components/pages/ProductPage/ProductPageComponents/SliderSection/SliderSection.jsx
+++ src/components/pages/ProductPage/ProductPageComponents/SliderSection/SliderSection.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './SliderSection.css'
+import React from "react";
+import "./SliderSection.css";
 
 export default function SliderSection() {
+  // create categoriesHandler function
+  const categoriesHandler = () => {};
+  
+  // create inputFilteredHandler function
+  const inputFilteredHandler = () => {};
+
   return (
-    <section className='slider-section'>
-        
+    <section className="slider-section">
+      <div className="categories-container">
+        <button className="new-categories" onClick={() => categoriesHandler()}>
+          New
+        </button>
+      </div>
+      <div className="filter-by-price">
+        <input
+          type="checkbox"
+          onChange={() => inputFilteredHandler()}
+          className="max-50"
+        />
+      </div>
+      <div className="ads-banner-container"></div>
     </section>
-  )
+  );
 }
