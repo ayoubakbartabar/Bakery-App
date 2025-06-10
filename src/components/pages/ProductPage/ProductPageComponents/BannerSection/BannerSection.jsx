95c60cd87882e42ffeeb446b7b219def1197a41a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-10 18:41:51 +0330

    Create BunnerSection components...

---
+++ src/components/pages/ProductPage/ProductPageComponents/BannerSection/BannerSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./BannerSection.css";
+
+export default function BannerSection() {
+  return (
+    <section className="banner-section">
+      <a href="/" className="banner banner-1"></a>
+      <a href="/" className="banner banner-2"></a>
+    </section>
+  );
+}
