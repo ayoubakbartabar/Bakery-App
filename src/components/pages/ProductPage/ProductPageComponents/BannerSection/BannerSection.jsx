38ea38a725bea4366db14563f1808cd0a3ca26be
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-11 15:39:05 +0330

    Update BannerSection.jsx and styles : add new tags and set new styles ..

--- src/components/pages/ProductPage/ProductPageComponents/BannerSection/BannerSection.jsx
+++ src/components/pages/ProductPage/ProductPageComponents/BannerSection/BannerSection.jsx
@@ -4,8 +4,12 @@ import "./BannerSection.css";
 export default function BannerSection() {
   return (
     <section className="banner-section">
-      <a href="/" className="banner banner-1"></a>
-      <a href="/" className="banner banner-2"></a>
+      <a href="/" className="banner banner-1">
+        <img src="src\assets\images\product-1.webp" alt="" />
+      </a>
+      <a href="/" className="banner banner-2">
+        <img src="src\assets\images\product-1.webp" alt="" />
+      </a>
     </section>
   );
 }
