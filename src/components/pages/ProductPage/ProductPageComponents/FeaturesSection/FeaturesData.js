d4ba9374e38251f955712f4f6f0ef1f5884c87d5
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-12 16:27:46 +0330

    Create FeaturesData.js

---
+++ src/components/pages/ProductPage/ProductPageComponents/FeaturesSection/FeaturesData.js
@@ -0,0 +1,5 @@
+const FeaturesData = [
+{id:1 , svg: "" , title:"Free Shipping" , detail:"ON ORDER OVER$199" , paragraph:"Want to track a package? Find tracking information and order details from Your Orders." , }
+]
+
+export default FeaturesData;
