597346daf78d921d4cf6dbc7e43b34a777562e8f
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-12 16:41:22 +0330

    Update FeaturesSection.jsx : create tags

--- src/components/pages/ProductPage/ProductPageComponents/FeaturesSection/FeaturesSection.jsx
+++ src/components/pages/ProductPage/ProductPageComponents/FeaturesSection/FeaturesSection.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import './FeaturesSection.css'
+import React from "react";
+import "./FeaturesSection.css";
+import FeaturesData from "./FeaturesData.js";
 
 export default function FeaturesSection() {
   return (
-    <section className='features-section'>FeaturesSection</section>
-  )
+    <section className="features-section">
+      {FeaturesData.map((item) => (
+        <div key={item.id} className="features-box">
+          <img src={item.svg} alt="svg" className="features-box-img" />
+          <h1 className="features-box-title">{item.title}</h1>
+          <p className="features-box-detail">{item.detail}</p>
+          <p className="features-box-paragraph">{item.paragraph}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
