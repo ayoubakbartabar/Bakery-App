042ccedcf4f1008ad04939780b44022c567add7e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-12 16:31:33 +0330

    Update FeaturesData.js : create properties .. :)

--- src/components/pages/ProductPage/ProductPageComponents/FeaturesSection/FeaturesData.js
+++ src/components/pages/ProductPage/ProductPageComponents/FeaturesSection/FeaturesData.js
@@ -1,5 +1,36 @@
 const FeaturesData = [
-{id:1 , svg: "" , title:"Free Shipping" , detail:"ON ORDER OVER$199" , paragraph:"Want to track a package? Find tracking information and order details from Your Orders." , }
-]
+  {
+    id: 1,
+    svg: "src/assets/images/fast-delivery.png",
+    title: "Free Shipping",
+    detail: "ON ORDER OVER$199",
+    paragraph:
+      "Want to track a package? Find tracking information and order details from Your Orders.",
+  },
+  {
+    id: 2,
+    svg: "src/assets/images/chef.png",
+    title: "Free Shipping",
+    detail: "ON ORDER OVER$199",
+    paragraph:
+      "Want to track a package? Find tracking information and order details from Your Orders.",
+  },
+  {
+    id: 3,
+    svg: "src/assets/images/flour.png",
+    title: "Free Shipping",
+    detail: "ON ORDER OVER$199",
+    paragraph:
+      "Want to track a package? Find tracking information and order details from Your Orders.",
+  },
+  {
+    id: 4,
+    svg: "src/assets/images/croissant.png",
+    title: "Free Shipping",
+    detail: "ON ORDER OVER$199",
+    paragraph:
+      "Want to track a package? Find tracking information and order details from Your Orders.",
+  },
+];
 
 export default FeaturesData;
