9c05fc767ad5422f1024612f64ead7777cfc5022
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-13 10:28:14 +0330

    Update BlogsPage.jsx : add FeaturesSection components ...

--- src/components/pages/BlogsPage/BlogsPage.jsx
+++ src/components/pages/BlogsPage/BlogsPage.jsx
@@ -1,14 +1,15 @@
+import { ProductInteractionProvider } from "../../shared/ProductInteractionContext/ProductInteractionContext";
 import React from "react";
 import "./BlogsPage.css";
 import Navbar from "../../shared/Navbar/Navbar";
 import Footer from "../../shared/Footer/Footer";
-import { ProductInteractionProvider } from "../../shared/ProductInteractionContext/ProductInteractionContext";
+import FeaturesSection from "../../shared/FeaturesSection/FeaturesSection";
 
 export default function BlogsPage() {
   return (
     <ProductInteractionProvider>
       <Navbar />
-
+      <FeaturesSection />
       <Footer />
     </ProductInteractionProvider>
   );
