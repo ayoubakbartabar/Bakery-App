f55a1193b87872f6b4feebc150b62bad57338fd3
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-14 16:47:56 +0330

    Update BlogsSection.jsx : create tags..

--- src/components/pages/BlogsPage/BlogsPageComponents/BlogsSection/BlogsSection.jsx
+++ src/components/pages/BlogsPage/BlogsPageComponents/BlogsSection/BlogsSection.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./BlogsSection.css";
+import BlogsData from "./BlogsData.js";
 
 export default function BlogsSection() {
   return (
-    <section className='blogs-section'>BlogsSection</section>
-  )
+    <section className="blogs-section">
+      {BlogsData.map((item) => (
+        <article key={item.id} className="blogs-card" aria-label={item.title}>
+          <img src={item.image} alt={item.title} className="blogs-image" />
+          <p className="blogs-date">{item.date}</p>
+          <h3 className="blogs-card-title">{item.title.toUpperCase()}</h3>
+          <p className="blogs-author">
+            By <span>{item.author}</span>
+          </p>
+          <p className="blogs-paragraph">{item.paragraph}</p>
+          <a
+            href="#"
+            className="read-more"
+            aria-label={`Read more about ${item.title}`}
+          >
+            READ MORE
+          </a>
+        </article>
+      ))}
+    </section>
+  );
 }
