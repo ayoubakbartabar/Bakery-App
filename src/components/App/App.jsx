3a24afde8a3af20c6ddf8acac0488034b2e0cba7
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-16 10:00:05 +0330

    Update App.jsx and ShoppingBagPage.jsx : edit routes in App.jsx and add new components to the ShoppingBagPage.jsx...

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,8 +4,8 @@ import "./App.css";
 import HomePage from "../pages/HomePage/HomePage.jsx";
 import AboutPage from "../pages/AboutPage/AboutPage.jsx";
 import BlogsPage from "../pages/BlogsPage/BlogsPage.jsx";
-import ContactPage from "../pages/ContactPage/ContactPage.jsx";
 import ProductPage from "../pages/ProductPage/ProductPage.jsx";
+import ShoppingBagPage from "../pages/ShoppingBagPage/ShoppingBagPage.jsx";
 
 function App() {
   return (
@@ -14,8 +14,7 @@ function App() {
       <Route path="/about" element={<AboutPage />} />
       <Route path="/product" element={<ProductPage />} />
       <Route path="/blogs" element={<BlogsPage/>} />
-      <Route path="/contact" element={<ContactPage/>} />
-      <Route path="/shoppingBag" element={<ContactPage/>} />
+      <Route path="/shoppingBag" element={<ShoppingBagPage/>} />
       <Route path="*" element={<HomePage/>} />
     </Routes>
   );
--- src/components/pages/ShoppingBagPage/ShoppingBagPage.jsx
+++ src/components/pages/ShoppingBagPage/ShoppingBagPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import './ShoppingBagPage.css'
+import React from "react";
+import "./ShoppingBagPage.css";
+import { ProductInteractionProvider } from "../../shared/ProductInteractionContext/ProductInteractionContext.jsx";
+import Navbar from "../../shared/Navbar/Navbar.jsx";
+import Footer from "../../shared/Footer/Footer.jsx";
 
 export default function ShoppingBagPage() {
   return (
-    <section className='shopping-bag-page'>ShoppingBagPage</section>
-  )
+    <ProductInteractionProvider>
+      <Navbar />
+
+      <Footer />
+    </ProductInteractionProvider>
+  );
 }
