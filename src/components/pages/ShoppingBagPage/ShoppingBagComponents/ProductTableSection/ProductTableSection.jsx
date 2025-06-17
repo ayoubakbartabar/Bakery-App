e66ac200ea502d888f43aab49697517e06be49c3
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-17 11:51:40 +0330

    Create ProductTableSection component...

---
+++ src/components/pages/ShoppingBagPage/ShoppingBagComponents/ProductTableSection/ProductTableSection.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ProductTableSection.css'
+
+export default function ProductTableSection() {
+  return (
+    <section className='product-table-section'>ProductTableSection</section>
+  )
+}
--- src/components/pages/ShoppingBagPage/ShoppingBagPage.jsx
+++ src/components/pages/ShoppingBagPage/ShoppingBagPage.jsx
@@ -3,6 +3,7 @@ import "./ShoppingBagPage.css";
 import { ProductInteractionProvider } from "../../shared/ProductInteractionContext/ProductInteractionContext.jsx";
 import Navbar from "../../shared/Navbar/Navbar.jsx";
 import ShoppingBagHeaderSection from "./ShoppingBagComponents/ShoppingBagHeaderSection/ShoppingBagHeaderSection.jsx";
+import ProductTableSection from "./ShoppingBagComponents/ProductTableSection/ProductTableSection.jsx";
 import Footer from "../../shared/Footer/Footer.jsx";
 
 export default function ShoppingBagPage() {
@@ -10,6 +11,7 @@ export default function ShoppingBagPage() {
     <ProductInteractionProvider>
       <Navbar />
         <ShoppingBagHeaderSection />
+        <ProductTableSection/>
       <Footer />
     </ProductInteractionProvider>
   );
