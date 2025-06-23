9f7720a181b288e3b7e1028bbf91f4aa07591ad5
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-23 19:49:02 +0330

    Update AboutNumberSection.jsx and style : create tags and set new syntax and styles ...

--- src/components/pages/AboutPage/AboutPageComponents/AboutNumberSection/AboutNumberSection.jsx
+++ src/components/pages/AboutPage/AboutPageComponents/AboutNumberSection/AboutNumberSection.jsx
@@ -11,16 +11,22 @@ export default function AboutNumberSection() {
   });
 
   return (
-    <section className="about-number-section" ref={ref}>
-      {AboutNumberData.map((item) => (
-        <div key={item.id} className="about-number-item">
-          <img src={item.svg} alt={item.title} className="about-number-image" />
-          <h3 className="about-number-number">
-            {inView ? <CountUp end={item.number} duration={2} /> : 0}
-          </h3>
-          <p className="about-number-title">{item.title}</p>
-        </div>
-      ))}
-    </section>
+    <div className="about-number-bg">
+      <section className="about-number-section" ref={ref}>
+        {AboutNumberData.map((item) => (
+          <div key={item.id} className="about-number-item">
+            <img
+              src={item.svg}
+              alt={item.title}
+              className="about-number-image"
+            />
+            <h3 className="about-number-number">
+              {inView ? <CountUp end={item.number} duration={2} /> : 0}
+            </h3>
+            <p className="about-number-title">{item.title}</p>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
