7b009f9a16e5fb6cf56ab29c98979d3207318cec
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-23 19:46:40 +0330

    Update AboutUsSection.jsx and style : create tags and set new syntax and styles ...

--- src/components/pages/AboutPage/AboutPageComponents/AboutUsSection/AboutUsSection.jsx
+++ src/components/pages/AboutPage/AboutPageComponents/AboutUsSection/AboutUsSection.jsx
@@ -3,14 +3,16 @@ import "./AboutUsSection.css";
 
 export default function AboutUsSection() {
   return (
-    <section className="about-us-section">
-      <h1 className="about-us-title">About Us</h1>
-      <span className="about-us-menu">
-        <a href="/" className="about-us-link">
-          HOME
-        </a>
-        <p className="about-us-paragraph">{` > About Us`}</p>
-      </span>
-    </section>
+    <div className="about-us-section-bg">
+      <section className="about-us-section">
+        <h1 className="about-us-title">About Us</h1>
+        <span className="about-us-menu">
+          <a href="/" className="about-us-link">
+            HOME
+          </a>
+          <p className="about-us-paragraph">{` > About Us`}</p>
+        </span>
+      </section>
+    </div>
   );
 }
