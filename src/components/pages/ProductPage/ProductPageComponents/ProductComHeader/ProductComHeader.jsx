390fc56a3c9e480abaee7bbb60a352c4cae7e265
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-24 11:20:17 +0330

    Update ProductComHeader.jsx and style : create tags and set new syntax and styles ...

--- src/components/pages/ProductPage/ProductPageComponents/ProductComHeader/ProductComHeader.jsx
+++ src/components/pages/ProductPage/ProductPageComponents/ProductComHeader/ProductComHeader.jsx
@@ -3,14 +3,16 @@ import "./ProductComHeader.css";
 
 export default function ProductComHeader() {
   return (
-    <section className="product-com-header">
-      <h1 className="product-header-title">Products</h1>
-      <span className="product-header-menu">
-        <a href="/" className="product-header-link">
-          HOME
-        </a>
-        <p className="product-header-paragraph">{` > Products`}</p>
-      </span>
-    </section>
+    <div className="product-com-header-bg">
+      <section className="product-com-header">
+        <h1 className="product-header-title">Products</h1>
+        <span className="product-header-menu">
+          <a href="/" className="product-header-link">
+            HOME
+          </a>
+          <p className="product-header-paragraph">{` > Products`}</p>
+        </span>
+      </section>
+    </div>
   );
 }
