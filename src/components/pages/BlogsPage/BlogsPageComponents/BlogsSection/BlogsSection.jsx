0c274bc37cc84218086d734d9ce31f68900d12c2
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-24 11:29:06 +0330

    Update BlogsSection.jsx and style : create tags and set new syntax and styles ...

--- src/components/pages/BlogsPage/BlogsPageComponents/BlogsSection/BlogsSection.jsx
+++ src/components/pages/BlogsPage/BlogsPageComponents/BlogsSection/BlogsSection.jsx
@@ -4,25 +4,27 @@ import BlogsData from "./BlogsData.js";
 
 export default function BlogsSection() {
   return (
-    <section className="blogs-section">
-      {BlogsData.map((item) => (
-        <article key={item.id} className="blogs-card" aria-label={item.title}>
-          <img src={item.image} alt={item.title} className="blogs-image" />
-          <p className="blogs-date">{item.date}</p>
-          <h3 className="blogs-card-title">{item.title.toUpperCase()}</h3>
-          <p className="blogs-author">
-            By <span>{item.author}</span>
-          </p>
-          <p className="blogs-paragraph">{item.paragraph}</p>
-          <a
-            href="#"
-            className="read-more"
-            aria-label={`Read more about ${item.title}`}
-          >
-            READ MORE
-          </a>
-        </article>
-      ))}
-    </section>
+    <div className="blogs-section-bg">
+      <section className="blogs-section">
+        {BlogsData.map((item) => (
+          <article key={item.id} className="blogs-card" aria-label={item.title}>
+            <img src={item.image} alt={item.title} className="blogs-image" />
+            <p className="blogs-date">{item.date}</p>
+            <h3 className="blogs-card-title">{item.title.toUpperCase()}</h3>
+            <p className="blogs-author">
+              By <span>{item.author}</span>
+            </p>
+            <p className="blogs-paragraph">{item.paragraph}</p>
+            <a
+              href="#"
+              className="read-more"
+              aria-label={`Read more about ${item.title}`}
+            >
+              READ MORE
+            </a>
+          </article>
+        ))}
+      </section>
+    </div>
   );
 }
