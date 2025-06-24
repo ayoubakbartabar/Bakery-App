d5f3850cd2ad02895fbf35a92f071e060f4139dc
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-24 11:33:08 +0330

    Update ShoppingBagHeaderSection.jsx and style : create tags and set new syntax and styles ...

--- src/components/pages/ShoppingBagPage/ShoppingBagComponents/ShoppingBagHeaderSection/ShoppingBagHeaderSection.jsx
+++ src/components/pages/ShoppingBagPage/ShoppingBagComponents/ShoppingBagHeaderSection/ShoppingBagHeaderSection.jsx
@@ -3,14 +3,16 @@ import "./ShoppingBagHeaderSection.css";
 
 export default function ShoppingBagHeaderSection() {
   return (
-    <section className="shopping-bag-header-section">
-      <h1 className="shopping-bag-header-title">Your Cart</h1>
-      <span className="shopping-bag-header-menu">
-        <a href="/" className="shopping-bag-header-link">
-          HOME
-        </a>
-        <p className="shopping-bag-header-paragraph">{` > Your Shopping Cart`}</p>
-      </span>
-    </section>
+    <div className="shopping-bag-header-section-bg">
+      <section className="shopping-bag-header-section">
+        <h1 className="shopping-bag-header-title">Your Cart</h1>
+        <span className="shopping-bag-header-menu">
+          <a href="/" className="shopping-bag-header-link">
+            HOME
+          </a>
+          <p className="shopping-bag-header-paragraph">{` > Your Shopping Cart`}</p>
+        </span>
+      </section>
+    </div>
   );
 }
