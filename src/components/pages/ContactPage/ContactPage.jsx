ae5eda4fabad1f3ccd6c9f68ac160030897edaa8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-28 09:06:05 +0330

    Create ContactMapSection component..

--- src/components/pages/ContactPage/ContactPage.jsx
+++ src/components/pages/ContactPage/ContactPage.jsx
@@ -4,12 +4,14 @@ import "./ContactPage.css";
 import Navbar from "../../shared/Navbar/Navbar";
 import ContactHeaderSection from "./ContactPageComponents/ContactHeaderSection/ContactHeaderSection";
 import ContactFormSection from "./ContactPageComponents/ContactFormSection/ContactFormSection";
+import ContactMapSection from "./ContactPageComponents/ContactMapSection/ContactMapSection";
 
 export default function ContactPage() {
   return (
     <ProductInteractionProvider>
       <Navbar />
       <ContactHeaderSection />
+      <ContactMapSection/>
       <ContactFormSection />
     </ProductInteractionProvider>
   );
---
+++ src/components/pages/ContactPage/ContactPageComponents/ContactMapSection/ContactMapSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ContactMapSection.css'
+
+export default function ContactMapSection() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
