e808f2f5b195a2e302cfd94c1481cc61576e6e98
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-28 09:31:10 +0330

    Update ContactPage.jsx : add other components ...

--- src/components/pages/ContactPage/ContactPage.jsx
+++ src/components/pages/ContactPage/ContactPage.jsx
@@ -5,14 +5,17 @@ import Navbar from "../../shared/Navbar/Navbar";
 import ContactHeaderSection from "./ContactPageComponents/ContactHeaderSection/ContactHeaderSection";
 import ContactFormSection from "./ContactPageComponents/ContactFormSection/ContactFormSection";
 import ContactMapSection from "./ContactPageComponents/ContactMapSection/ContactMapSection";
-
+import FeaturesSection from "../../shared/FeaturesSection/FeaturesSection";
+import Footer from "../../shared/Footer/Footer";
 export default function ContactPage() {
   return (
     <ProductInteractionProvider>
       <Navbar />
       <ContactHeaderSection />
-      <ContactMapSection/>
       <ContactFormSection />
+      <FeaturesSection />
+      <ContactMapSection />
+      <Footer />
     </ProductInteractionProvider>
   );
 }
