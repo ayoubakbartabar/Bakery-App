ee0b4c640a0abfb6ff8a06cced2d535026ec7388
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-06-30 10:16:54 +0330

    Update index.css and CarouselSectionData.js : edite images root and fonts root ...

--- src/components/shared/CarouselSection/CarouselSectionData.js
+++ src/components/shared/CarouselSection/CarouselSectionData.js
@@ -1,7 +1,7 @@
 const CarouselSectionData = [
   {
     id: 1,
-    image: "src/assets/images/1_152x152_crop_center.avif",
+    image: "/images/1_152x152_crop_center.avif",
     allstars: 5,
     rate: 4,
     text: "“Dessert pudding dessert jelly beans cupcake sweet caramels gingerbread. Fruitcake biscuit cheesecake. Cookie topping sweet muffin pudding tart bear claw sugar plum croissant.”",
@@ -10,7 +10,7 @@ const CarouselSectionData = [
   },
   {
     id: 2,
-    image: "src/assets/images/2_152x152_crop_center.avif",
+    image: "/images/2_152x152_crop_center.avif",
     allstars: 5,
     rate: 5,
     text: "“Dessert pudding dessert jelly beans cupcake sweet caramels gingerbread. Fruitcake biscuit cheesecake. Cookie topping sweet muffin pudding tart bear claw sugar plum croissant.”",
@@ -19,7 +19,7 @@ const CarouselSectionData = [
   },
   {
     id: 3,
-    image: "src/assets/images/4_152x152_crop_center.avif",
+    image: "/images/4_152x152_crop_center.avif",
     allstars: 5,
     rate: 5,
     text: "“Dessert pudding dessert jelly beans cupcake sweet caramels gingerbread. Fruitcake biscuit cheesecake. Cookie topping sweet muffin pudding tart bear claw sugar plum croissant.”",
